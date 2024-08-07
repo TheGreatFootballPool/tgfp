970221b498f3fbd56f012536432d92524633d099
John Sturgeon <john@sturgeon.me>
2024-08-06 20:22:34 -0700

    Added pylint, flake8 and pytest
    Resolves #5

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+""" Main entry point for web site """
 from typing import Union
 
 from fastapi import FastAPI
@@ -7,9 +8,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """ Hello World """
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    """ Read Item from DB """
     return {"item_id": item_id, "q": q}
